0da4182c404661209156e2d0a387ea53f7369d88
AhnellCSP <93556733+AhnellCSP@users.noreply.github.com>
2022-06-12 17:05:03 -0500

    Update ZombieWar.java
    Added options to menu() function.
    
    Option 2: display description of game
    
    Option 3: Exit
    
    If invaild user input, user is prompted to select an option from the list.
    
    Menu loops until "Exit" is selected.
    
    Changed input variable from int to String.

--- ZombieWar.java
+++ ZombieWar.java
@@ -9,19 +9,55 @@ public class ZombieWar {
     public Group faction2;
     Random rand = new Random();
 
-    // A menu with currently 1 choice to select from
+    /**
+     * A menu to navigate the ZombieWar game choices. User may start run of 
+     * ZombieWar, Display a description of the game, or exit. Will prompt for
+     * valid entry if unable to decipher user selection.
+     */
     public void menu() {
-        System.out.println("Please select your options.\n 1)Start Game");
+        boolean useMenu = true;
         Scanner input = new Scanner(System.in);
-        int choice = input.nextInt();
-        switch (choice) {
-            case 1:
-                // creates an instance of the battle manager
-                currentBattle = doZombieWar();
-                // plays the game
-                currentBattle.play();
+        
+        while (useMenu) {
+            System.out.print("\nSelect your option."
+                + "\n 1)Start Game"
+                + "\n 2)Game Description"
+                + "\n 3)Exit\n");
+            
+            
+            String choice = input.nextLine();
+            switch (choice) {
+                case "1":
+                    // creates an instance of the battle manager
+                    currentBattle = doZombieWar();
+                    // plays the game
+                    currentBattle.play();
+                    break;
+                case "2":
+                    // Display Brief description of game
+                    System.out.println("\nZombieWar\n"
+                            + "\n"
+                            + "In this brief simulation of a human/zombie war, \n"
+                            + "two factions--one of dauntless human fighters, one \n"
+                            + "of shambling zombies--do battle to see who will prevail."
+                            + "\n\n"
+                            + "Who will make it to the other side?\n");
+                    break;
+                case "3":
+                    // Exit game
+                    System.out.println("Thank you for playing the ZombieWar."
+                            + "\nTake care of your brains.");
+                    useMenu = false;
+                    break;
+                default:
+                    // invalid input from user, display menu again
+                    System.out.println("Please select an option from the list.");
+                    break;
+            }
+            
         }
         input.close();
+        
     }
 
     // generates and instance of the battle manager and generates the factions
