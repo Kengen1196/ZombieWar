2a65feef7fd60b9109cf0a97bd493bd31374f838
AhnellCSP <93556733+AhnellCSP@users.noreply.github.com>
2022-06-25 19:00:17 -0500

    Update ZombieWar.java
    Additional menu options for Zombie War.
    
    Combatant Stats, listing Combatant types, starting health and base attack power.
    
    Weapon Stats, listing Weapon types, accuracy and attack power.

--- ZombieWar.java
+++ ZombieWar.java
@@ -13,6 +13,8 @@ public class ZombieWar {
      * A menu to navigate the ZombieWar game choices. User may start run of
      * ZombieWar, Display a description of the game, or exit. Will prompt for
      * valid entry if unable to decipher user selection.
+     * 
+     * For Release 3 Added Combatant and Weapon Statistics menu options
      */
     public void menu() {
         boolean useMenu = true;
@@ -22,7 +24,9 @@ public class ZombieWar {
             System.out.print("\nSelect your option."
                     + "\n 1)Start Game"
                     + "\n 2)Game Description"
-                    + "\n 3)Exit\n");
+                    + "\n 3)Combatant Stats"
+                    + "\n 4)Weapon Stats"
+                    + "\n 5)Exit\n");
 
             String choice = input.nextLine();
             switch (choice) {
@@ -43,6 +47,30 @@ public class ZombieWar {
                             + "Who will make it to the other side?\n");
                     break;
                 case "3":
+                    // Display description and stats of Combatant types
+                    System.out.println("Human Combatants:\n");
+                    System.out.println("** Mercenary       **   Starting health: 100   Attack: 10");
+                    System.out.println("** Newbie          **   Starting health: 20    Attack: 2");
+                    System.out.println("** Soldier         **   Starting health: 50    Attack: 5");
+                    System.out.println();
+                    System.out.println("Zombie Combatants:\n");
+                    System.out.println("** Common Infected **   Starting health: 30    Attack: 5");
+                    System.out.println("** Tank            **   Starting health: 150   Attack: 20");
+                    break;
+                case "4":
+                    // Display description and stats of Weapons
+                    System.out.println("Weapon Statistics:\n");
+                    System.out.println("** Assault Rifle  **   Accuracy Rating: 80   Attack: 3");
+                    System.out.println("** Axe            **   Accuracy Rating: 90   Attack: 6");                    
+                    System.out.println("** Crowbar        **   Accuracy Rating: 80   Attack: 4");
+                    System.out.println("** Frying Pan     **   Accuracy Rating: 80   Attack: 2");
+                    System.out.println("** Mouth          **   Accuracy Rating: 80   Attack: 0");
+                    System.out.println("** Pistol         **   Accuracy Rating: 70   Attack: 2");
+                    System.out.println("** Shotgun        **   Accuracy Rating: 60   Attack: 6");
+                    System.out.println("** Submachine Gun **   Accuracy Rating: 45   Attack: 2");
+                    System.out.println();
+                    break;
+                case "5":
                     // Exit game
                     System.out.println("Thank you for playing the ZombieWar."
                             + "\nTake care of your brains.");
